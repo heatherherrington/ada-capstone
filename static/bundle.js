65ae4d3f597a7f2b32539e949450ca59bafd6626
heatherherrington <redherring@gmail.com>
2017-01-13 15:26:59 -0800

    Change to bundle.js

--- static/bundle.js
+++ static/bundle.js
@@ -69,11 +69,11 @@
 
 	var _animals2 = _interopRequireDefault(_animals);
 
-	var _animal = __webpack_require__(230);
+	var _animal = __webpack_require__(228);
 
 	var _animal2 = _interopRequireDefault(_animal);
 
-	var _sanctuary = __webpack_require__(228);
+	var _sanctuary = __webpack_require__(229);
 
 	var _sanctuary2 = _interopRequireDefault(_sanctuary);
 
@@ -25811,6 +25811,45 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var Animal = _react2.default.createClass({
+	  displayName: 'Animal',
+	  componentDidMount: function componentDidMount() {
+	    this.setState({
+	      // route components are rendered with useful information, like URL params
+	      animal: findAnimalById(this.props.params.animalId)
+	    });
+	  },
+	  render: function render() {
+	    return _react2.default.createElement(
+	      'div',
+	      null,
+	      _react2.default.createElement(
+	        'h2',
+	        null,
+	        this.state.animal.name
+	      )
+	    );
+	  }
+	});
+
+	exports.default = Animal;
+
+/***/ },
+/* 229 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _react = __webpack_require__(2);
+
+	var _react2 = _interopRequireDefault(_react);
+
 	var _reactDom = __webpack_require__(159);
 
 	var _reactDom2 = _interopRequireDefault(_reactDom);
@@ -25829,7 +25868,7 @@
 
 	var _main2 = _interopRequireDefault(_main);
 
-	var _jquery = __webpack_require__(229);
+	var _jquery = __webpack_require__(230);
 
 	var _jquery2 = _interopRequireDefault(_jquery);
 
@@ -25899,7 +25938,7 @@
 	});
 
 /***/ },
-/* 229 */
+/* 230 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -36123,45 +36162,6 @@
 	return jQuery;
 	} );
 
-
-/***/ },
-/* 230 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-
-	var _react = __webpack_require__(2);
-
-	var _react2 = _interopRequireDefault(_react);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	var Animal = _react2.default.createClass({
-	  displayName: 'Animal',
-	  componentDidMount: function componentDidMount() {
-	    this.setState({
-	      // route components are rendered with useful information, like URL params
-	      animal: findAnimalById(this.props.params.animalId)
-	    });
-	  },
-	  render: function render() {
-	    return _react2.default.createElement(
-	      'div',
-	      null,
-	      _react2.default.createElement(
-	        'h2',
-	        null,
-	        this.state.animal.name
-	      )
-	    );
-	  }
-	});
-
-	exports.default = Animal;
 
 /***/ }
 /******/ ]);
