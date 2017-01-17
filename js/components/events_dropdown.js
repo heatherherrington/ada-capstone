f2ddc8e28ce8a9e158d3c9bc39eae3c4c197c95a
heatherherrington <redherring@gmail.com>
2017-01-16 17:52:02 -0800

    Created dropdown component

---
+++ js/components/events_dropdown.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AnimalEvent from '/animal_event'
+
+class EventsDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default EventsDropdown;
