e7a1210886f5270d5c35ccbefd4cd54e04c64f42
heatherherrington <redherring@gmail.com>
2017-01-20 16:55:26 -0800

    Trying to get Google calendar API working

--- js/components/animal_add.js
+++ js/components/animal_add.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
 export default React.createClass({
+  // getInitialState: function() {
+  //   return { name: null }
+  // },
+
   handleClick() {
+    // var that = this;
+
     var name = this.refs.name.value;
     $.ajax({
       url: '/animal',
       type: 'POST',
       data: {name: name},
     });
+    // that.setState(name: name)
   },
 
   render() {
