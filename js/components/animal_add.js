f95dfb0c6d2af5e6469483797ad169048a77c90b
heatherherrington <redherring@gmail.com>
2017-01-22 14:22:21 -0800

    Auto refreshes when new animal is added

--- js/components/animal_add.js
+++ js/components/animal_add.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
 export default React.createClass({
@@ -8,22 +7,21 @@ export default React.createClass({
   // },
 
   handleClick() {
-    // var that = this;
-
-    var name = this.refs.name.value;
+    let callback = this.props.onAdd;
+    let name = this.refs.name.value;
     $.ajax({
       url: '/animal',
       type: 'POST',
       data: {name: name},
+      complete: callback,
     });
-    // that.setState(name: name)
   },
 
   render() {
     return (
       <div>
-        <input ref='name' placeholder='Enter the name of your animal' />
-          <button onClick={this.handleClick}>Submit</button>
+        <input ref='name' placeholder='Enter the name of your animal'/>
+        <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
--- js/sanctuary.js
+++ js/sanctuary.js
@@ -1,55 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Header from './components/header';
 import Footer from './components/footer';
 import { Link } from 'react-router';
-import Main from './components/main';
 import AnimalAdd from './components/animal_add'
 import $ from 'jquery';
 
-export default React.createClass({
+const Sanctuary = React.createClass({
 
   getInitialState: function() {
     return { animals: [] }
   },
 
+  refreshFromServer: function () {
+    let self = this;
+    $.getJSON('/sanctuary/api/animals',
+      function (response) {
+        self.setState({animals: response.animals});
+      }
+    );
+  },
+
   componentDidMount() {
-    var that = this;
+    this.refreshFromServer();
+  },
 
-    $.getJSON('http://localhost:5000/sanctuary/api/animals',
-    function(response) {
-      that.setState({ animals: response.animals })
-    }
-  )},
-
-  // componentWillMount() {
-  //   const id = setInterval(this.fetchData, 5000);
-  //   this.setState({intervalId: id});
-  // },
-  //
-  // fetchData() {
-  //   var that = this;
-  //
-  //   $.getJSON('http://localhost:5000/sanctuary/api/animals',
-  //   function(response) {
-  //     that.setState({ animals: response.animals })
-  //   }
-  // )},
-
-  runRender: function() {
-    if(this.state.animals[0] != null) {
+  runRender: function () {
+    if (this.state.animals[0] != null) {
       var animals = this.state.animals.map((animal) =>
-        <div key={animal.id}>
-          <li><Link to={`/animal/${animal.id}`}>{ animal.name }</Link></li>
-        </div>
-      );
-      return (
-        <ul>{animals}</ul>
-      )
-    }
+      <div key={animal.id}>
+        <li><Link to={`/animal/${animal.id}`}>{ animal.name }</Link></li>
+      </div>
+    );
+    return (
+      <ul>{animals}</ul>
+    )
+    };
   },
 
   render() {
+    let self = this;
+    let onAdd = function () {
+      console.log("Sanctuary onAdd callback called.");
+      self.refreshFromServer();
+    };
     console.log("state.animals", this.state.animals);
 
     return (
@@ -57,9 +50,11 @@ export default React.createClass({
         <Header />
         <h1>Hope Haven Farm Sanctuary</h1>
         { this.runRender() }
-        <AnimalAdd />
+        <AnimalAdd onAdd={onAdd}/>
         <Footer />
       </div>
     )
   }
-})
+});
+
+export default Sanctuary;
